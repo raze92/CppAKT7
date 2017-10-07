7033e3142be1534048bad17cb11d885d077f249c
Michael Radzieda <michael.radzieda@web.de>
2017-10-07 16:09:43 +0200

    Initial Commit

---
+++ AKT7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AKT7
+//
+//  Created by Michael Radzieda on 07.10.17.
+//  Copyright © 2017 de.TeamBox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
